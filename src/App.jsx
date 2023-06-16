c6009c451ccda33eb0dc3e8a6aaabce1e244edd8
kwzh <wzh.k@outlook.com>
2023-06-16 20:40:39 +0800

    upgrade @solid-primitives/websocket to 1.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
 import Scene from "./Scene";
 import {createSignal} from "solid-js";
-import createWebsocket from "@solid-primitives/websocket";
+import {createWS} from "@solid-primitives/websocket";
 
-const example_data = {
-  "action": "sent",
-  "prediction": [[3.8975167274475098, 7.749725341796875, 6.8757524490356445, 6.921577453613281, 7.2840256690979]]
-}
+// const example_data = {
+//   "action": "sent",
+//   "prediction": [[3.8975167274475098, 7.749725341796875, 6.8757524490356445, 6.921577453613281, 7.2840256690979]]
+// }
 
 function App() {
   let id = 0
 
   const [data, setData] = createSignal([]);
-  const [connect, disconnect, send, state] = createWebsocket(
-    "ws://localhost:8081/infer/" + id,
-    (msg) => {
-      const response = JSON.parse(msg.data)
-      setData(response.prediction)
-    },
-    (msg) => console.log(msg),
-    [],
-    5,
-    5000
-  );
+  const ws = createWS("ws://localhost:8081/infer/" + id)
+  ws.addEventListener("message", (ev) => {
+    const response = JSON.parse(ev.data)
+    setData(response.prediction)
+  });
 
-  connect()
 
   return (
     <Scene curls={data}/>
